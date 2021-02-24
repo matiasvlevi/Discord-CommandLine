6b009abd26d854c1195de78e9e1b2e4fb7601f9a
matiasvlevi <matiasvlevi@gmail.com>
2021-02-24 13:30:42 -0500

    init

--- build/index.js
+++ build/index.js
@@ -7,11 +7,6 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-// TEST COMMAND:
-commands.add('ping',() => (
-    'pong'
-));
-
 // RUN CMD COMMAND:
 commands.add('cmd', (arg) => {
     //Combine arguments to create a batch command
